5a6e5e4f10562e61da53dcfb3d376d0d8b6d4693
NknightA <n.knight.pc0627@gmail.com>
2023-03-01 22:04:40 +0900

    edit 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Coc404() {
+	return (
+		<div style={{
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			height: 600,
+			color: '#fff'
+		}}>
+			page not found
+			<div>
+				<a href='/'><span>→</span>backhome</a>
+			</div>
+		</div>
+	)
+}
