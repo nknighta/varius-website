40f58090d6c46f6bddde6196955eb4b8e73257ad
NknightA <n.knight.pc0627@gmail.com>
2023-03-09 18:49:35 +0900

    update.

--- src/pages/blog/2023-03/index.tsx
+++ src/pages/blog/2023-03/index.tsx
@@ -1,6 +1,7 @@
 import {Text,Box} from "@chakra-ui/react";
 
 export default function Index () {
+    const Turn = "\n";
     return (
         <>
             <div style={{display: 'flex'}}>
@@ -25,7 +26,7 @@ export default function Index () {
                 <h3>Edit by NknightAMAMIYA - VARIUS owner</h3>
                 <Box style={{whiteSpace:"pre-wrap"}}>
                     <Text fontSize={22}>{`   みなさんこんばんは、あまみやです。VARIUSのブログをご覧いただきありがとうございます。`}</Text>
-                    <Text fontSize={22}>{`本当はこのブログも英語で書こうかと思ったんですが、英訳が面倒なのと正確に翻訳できる自信がなかったので日本語で書きます(笑)s`}</Text>
+                    <Text fontSize={22}>{`本当はこのブログも英語で書こうかと思ったんですが、英訳が面倒なのと正確に翻訳できる自信がなかったので日本語\nで書きます(笑)s`}</Text>
                     <Text fontSize={22}>{`当サイトはWeb3やWeb5の開発者やクリエイターの方々に向けて、プラグイン等の提供による活動の手助けになればと思いつくりました。`}</Text>
                     <Text fontSize={42}>{`これからやりたいこと`}</Text>
                     <Text fontSize={22}>{` このサイトのロードマップに関しては、現在ProjectVとユーザー管理システム、それから各種アプリケーションのフォーマット決めを行っており最終的には今年の秋頃の本格リリースをめざしております。`}</Text>
